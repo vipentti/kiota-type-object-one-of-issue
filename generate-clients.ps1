7c40d645bbbefd6b1c198d3ab9e57398d8a64789
Ville Penttinen <villem.penttinen@gmail.com>
2024-12-19 06:56:39 +0200

    Add new problem sample

--- generate-clients.ps1
+++ generate-clients.ps1
@@ -7,6 +7,7 @@ $root = $PSScriptRoot
 
 $SimpleShemaFile = Join-Path -Path $root -ChildPath "DiscriminatorProblemSampleSimple.yaml" -Resolve
 $ComplexShemaFile = Join-Path -Path $root -ChildPath "DiscriminatorProblemSampleComplex.yaml" -Resolve
+$HierarchyShemaFile = Join-Path -Path $root -ChildPath "DiscriminatorProblemSampleHierarchy.yaml" -Resolve
 
 # <CLI|CSharp|Go|Java|PHP|Python|Ruby|Swift|TypeScript>
 
@@ -22,6 +23,7 @@ $LanguagesToGenerate = @(
 )
 
 foreach ($lang in $LanguagesToGenerate) {
-    & kiota generate -d $SimpleShemaFile -l $lang -o ./Generated/Simple/$lang --clean-output
-    & kiota generate -d $ComplexShemaFile -l $lang -o ./Generated/Complex/$lang --clean-output
+    & dotnet tool run kiota generate -d $SimpleShemaFile -l $lang -o ./Generated/Simple/$lang --clean-output
+    & dotnet tool run kiota generate -d $ComplexShemaFile -l $lang -o ./Generated/Complex/$lang --clean-output
+    & dotnet tool run kiota generate -d $HierarchyShemaFile -l $lang -o ./Generated/Hierarchy/$lang --clean-output
 }
