79d481952c91f929a4d2e27836776009744cdfb8
Ville Penttinen <villem.penttinen@gmail.com>
2024-12-19 06:55:51 +0200

    Re-generate problems with updated kiota to verify fixes

--- Generated/Simple/CSharp/Models/ExampleWithSingleOneOfWithTypeObject.cs
+++ Generated/Simple/CSharp/Models/ExampleWithSingleOneOfWithTypeObject.cs
@@ -14,6 +14,22 @@ namespace ApiSdk.Models
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>The objectType property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? ObjectType { get; set; }
+#nullable restore
+#else
+        public string ObjectType { get; set; }
+#endif
+        /// <summary>The one property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? One { get; set; }
+#nullable restore
+#else
+        public string One { get; set; }
+#endif
         /// <summary>
         /// Instantiates a new <see cref="global::ApiSdk.Models.ExampleWithSingleOneOfWithTypeObject"/> and sets the default values.
         /// </summary>
@@ -39,6 +55,8 @@ namespace ApiSdk.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "objectType", n => { ObjectType = n.GetStringValue(); } },
+                { "one", n => { One = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -48,6 +66,8 @@ namespace ApiSdk.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
+            writer.WriteStringValue("objectType", ObjectType);
+            writer.WriteStringValue("one", One);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
